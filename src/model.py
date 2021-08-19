8b5029c58187645e7192a65111fd856999b63722
Ahmed A. A. Elhag <38051706+Ahmed-A-A-Elhag@users.noreply.github.com>
2021-08-19 22:14:05 +0000

    Update model.py

--- src/model.py
+++ src/model.py
@@ -11,7 +11,6 @@ class GCN(torch.nn.Module):
     def forward(self, fts, adj):
         fts = torch.nn.functional.relu(self.gc1(fts, adj))
         fts = torch.nn.functional.dropout(fts, self.dropout, training=self.training)
-        #fts = torch.nn.functional.dropout(fts, p=0.5, training=True)
         fts = self.gc2(fts, adj)
         return torch.nn.functional.log_softmax(fts, dim=1)
       
